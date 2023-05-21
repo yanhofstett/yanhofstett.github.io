50ba483d5899ee927d9ccc0ae4140647438527e2
unknown <pw42mwi@eduvaud.ch>
2023-05-21 19:43:14 +0200

    V5

--- JavaScript/hide.js
+++ JavaScript/hide.js
@@ -2,19 +2,22 @@
 var currentDate = new Date();
 
 // Définir la date et l'heure de référence (20 mai 2023 à 8h45)
-var referenceDate = new Date(2023, 4, 21, 14, 32);
+var referenceDate = new Date(2023, 4, 21, 18, 30);
 
 // Vérifier si la date et l'heure actuelles sont après la date de référence
 if (currentDate > referenceDate) {
-// Afficher les éléments correspondants aux jours 1, 2 et 3
-document.getElementById('jour1').style.display = 'list-item';
-document.getElementById('jour2').style.display = 'list-item';
-document.getElementById('jour3').style.display = 'list-item';
+    // Afficher les éléments correspondants aux jours 1, 2 et 3
+    document.getElementById('jour1').style.display = 'list-item';
+    document.getElementById('jour2').style.display = 'list-item';
+    document.getElementById('jour3').style.display = 'list-item';
+    document.getElementById('jour4').style.display = 'list-item';
+    document.getElementById('shopping').style.display = 'list-item';
 }
 else
 {
-document.getElementById('jour1').style.display = 'none';
-document.getElementById('jour2').style.display = 'none';
-document.getElementById('jour3').style.display = 'none';
-
+    document.getElementById('jour1').style.display = 'none';
+    document.getElementById('jour2').style.display = 'none';
+    document.getElementById('jour3').style.display = 'none';
+    document.getElementById('jour4').style.display = 'none';
+    document.getElementById('shopping').style.display = 'none';
 }
